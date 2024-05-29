744dadac711093b1cc4c865bf022adf09f7cce1d
Agustin Cacciamani <agucacciamani@gmail.com>
2024-05-29 17:34:01 -0300

    Añadiendo cards

--- src/components/ItemListContainerComponent/ItemListContainerComponent.jsx
+++ src/components/ItemListContainerComponent/ItemListContainerComponent.jsx
@@ -1,19 +1,44 @@
+import React, { useEffect } from 'react';
+
 import { height } from '@fortawesome/free-brands-svg-icons/fa42Group';
-
-import React from 'react';
-
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import CountComponent from '../CountComponent/CountComponent';
 import "./ItemListContainerComponent.css";
-
+import { getAllProducts } from '../../services/productsServices';
 
 const ItemListContainerComponent = ({ greeting }) => {
-  
+
+  const [products, setProducts] = React.useState([]);
+
+  React.useEffect(() => {
+    getAllProducts()
+      .then((res) => setProducts(res.data.products))
+      .catch((err) => console.log(err))
+  }, []);
+
 
   return (
     <>
       <CountComponent />
       <div className='itemListContainerComponent'>
         {greeting}
+      </div>
+      <div>
+        {products.map((products) => {
+          return (
+            <Card key={products.id} style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={products.thumbnail} />
+              <Card.Body>
+                <Card.Title>{products.title}</Card.Title>
+                <Card.Text>
+                  {products.description}
+                </Card.Text>
+                <Button variant="primary">Ir al detalle</Button>
+              </Card.Body>
+            </Card>
+          );
+        })}
       </div>
     </>
   );
